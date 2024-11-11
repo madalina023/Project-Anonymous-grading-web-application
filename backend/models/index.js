b8f741900f399af8ad89a08f1a492d0f20745999
Mădălina Demian <68812363+madalina023@users.noreply.github.com>
2024-11-11 15:21:34 +0200

    Update index.js

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 const db = require("../config/db");
-// Models
+ 
 const StudentModel = require("./student");
 const TeacherModel = require("./teacher");
 const ProjectModel = require("./project");
@@ -13,13 +13,7 @@ const Project = ProjectModel(db, Sequelize);
 const PartialDeliverable = PartialDeliverableModel(db, Sequelize);
 const Grade = GradeModel(db, Sequelize);
 
-// Relatii tabele
-// Teacher.hasMany(Student, {
-//   foreignKey: "teacherId",
-//   as: "Student",
-// });
-// Student.belongsTo(Teacher);
-
+ 
 Student.hasMany(Project, {
   foreignKey: "studentId",
   as: "Project",
